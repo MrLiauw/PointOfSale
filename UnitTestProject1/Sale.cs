f08b1fa8815ef27b438f5780c93f4a163a237dcc
MrLiauw <maryantoliauw@gmail.com>
2016-07-11 15:16:31 +0800

    we can handle missing barcode product now

--- UnitTestProject1/Sale.cs
+++ UnitTestProject1/Sale.cs
@@ -16,7 +16,7 @@
             else if(barcode == "23456")
                 display.Text = "$12.50";
             else
-                display.Text = "Product not found for 99999";
+                display.Text = "Product not found for " + barcode;
         }
     }
 }

ce680635742dc9490a15fb2df440f200205b1ad8
MrLiauw <maryantoliauw@gmail.com>
2016-07-15 12:20:32 +0800

    we can sell a single item at any price

--- UnitTestProject1/Sale.cs
+++ UnitTestProject1/Sale.cs
@@ -37,7 +37,7 @@ namespace UnitTestProject1
                 display.DisplayNoSaleInProgressMessage();
             else
             {
-                display.Text = "Total: $6.50";
+                display.Text = "Total: " + price;
             }
         }
     }

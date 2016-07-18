f4d7d5dfe1eab0755dd44375946c37aeb197ce5f
MrLiauw <maryantoliauw@gmail.com>
2016-07-18 18:53:27 +0800

    prepared to introduce parameterized test for formatting monetary amount

---
+++ UnitTestProject1/FormatMonetaryAmountTest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class FormatMonetaryAmountTest
+    {
+        [TestMethod]
+        public void simplest()
+        {
+            Assert.AreEqual("$7.89", format(789));
+        }
+
+        private static string format(int priceInCents)
+        {
+            return "$7.89";
+        }
+    }
+}

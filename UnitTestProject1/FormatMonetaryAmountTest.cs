58d930f43e14b2ef1b55147a24b4fea68dc555e1
MrLiauw <maryantoliauw@gmail.com>
2016-07-18 19:16:22 +0800

    our parameterized test now better identify itself when it failed

--- UnitTestProject1/FormatMonetaryAmountTest.cs
+++ UnitTestProject1/FormatMonetaryAmountTest.cs
@@ -6,7 +6,7 @@ namespace UnitTestProject1
     public class FormatMonetaryAmountTest
     {
         [Test]
-        [TestCase("$7.89", 789)]
+        [TestCase("$7.89", 789, TestName = "Monetary amount of 789 formatted to $7.89")]
         public void simplest(string expected, int priceWithCents)
         {
             Assert.AreEqual(expected, format(priceWithCents));

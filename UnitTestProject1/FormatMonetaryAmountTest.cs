faa79844a8d0bc85de18d0e0953bc8ca6b1b62b5
MrLiauw <maryantoliauw@gmail.com>
2016-07-18 19:32:45 +0800

    we can now format prices with leading zeroes (less than a dollar)

--- UnitTestProject1/FormatMonetaryAmountTest.cs
+++ UnitTestProject1/FormatMonetaryAmountTest.cs
@@ -7,6 +7,10 @@ namespace UnitTestProject1
     {
         [TestCase("$7.89", 789, TestName = "Monetary amount of 789 formatted to $7.89")]
         [TestCase("$5.20", 520, TestName = "Monetary amount of 520 formatted to $5.20")]
+        [TestCase("$4.00", 400, TestName = "Monetary amount of 400 formatted to $4.00")]
+        [TestCase("$0.00", 0, TestName = "Monetary amount of 0 formatted to $0.00")]
+        [TestCase("$0.02", 2, TestName = "Monetary amount of 2 formatted to $0.02")]
+        [TestCase("$0.37", 37, TestName = "Monetary amount of 37 formatted to $0.37")]
         public void Simplest(string expected, int priceWithCents)
         {
             Assert.AreEqual(expected, format(priceWithCents));
@@ -15,7 +19,7 @@ namespace UnitTestProject1
         private static string format(int priceInCents)
         {
             decimal price = (decimal)priceInCents/100;
-            return string.Format("${0}", price.ToString("#.00"));
+            return string.Format("${0}", price.ToString("0.00"));
         }
     }
 }
